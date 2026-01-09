6a565857f1b0372cd16b19710fc98f16d118acc5
Murosha <mz5145282@gmail.com>
2026-01-09 22:41:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-document.querySelectorAll(`.word`).forEach
-(word => {
-    const en = word.textContent
-    const ua = word.dataset.ua
-    word.addEventListener(`click`, () => {
-        word.textContent = word.textContent === en ? ua : en;
-    });
+const buttons = document.querySelectorAll("nav button");
+const container = document.getElementById("words");
+
+buttons.forEach(button => {
+  button.addEventListener("click", async () => {
+    const fileName = button.dataset.file;
+    console.log("Натиснув кнопку:", fileName)
+    const response = await fetch(fileName);
+    const text = await response.text();
+    console.log("Отриманний текст:", text);
+    renderWords(text);
+  });
 });
+
+function renderWords(text) {
+  container.innerHTML = "";
+  const lines = text.split("\n");
+
+  lines.forEach(line => {
+    if (!line.includes("/")) return;
+
+    const lastSlash = line.lastIndexOf("/");
+if (lastSlash === -1) return;
+
+const en = line.slice(0, lastSlash).trim();
+const ua = line.slice(lastSlash + 1).trim();
+
+
+    const p = document.createElement("p");
+    p.className = "word";
+    p.dataset.ua = ua;
+    p.textContent = en;
+    p.addEventListener("click", () => {
+        p.textContent = p.textContent === en ? ua : en;
+    });
+    container.appendChild(p);
+  });
+}
+
 
 
 
