37bb483154d669c2b343fe0db0bc8f29f5282fa1
Murosha <mz5145282@gmail.com>
2026-01-11 21:36:18 +0200

    Update IT.js

--- IT.js
+++ IT.js
@@ -1,5 +1,16 @@
-const btn = document.getElementById("btnIT");
-const itcontainer = document.getElementById("itwords");
+const buttons = document.querySelectorAll("nav button");
+const container = document.getElementById("itwords");
+
+buttons.forEach(button => {
+  button.addEventListener("click", async () => {
+    const fileName = button.dataset.file;
+    console.log("Натиснув кнопку:", fileName)
+    const response = await fetch(fileName);
+    const text = await response.text();
+    console.log("Отриманний текст:", text);
+    renderWords(text);
+  });
+});
 
 function renderWords(text) {
   container.innerHTML = "";
@@ -8,19 +19,31 @@ function renderWords(text) {
   lines.forEach(line => {
     line = line.trim();
     if (!line) return;
+    if (/^\d+\s*[A-Z]/i.test(line)) {
+      const clean = line.match(/^\d+\s*[A-Z]/i)[0];
+
+      const h2 = document.createElement("h2");
+      h2.textContent = clean.replace(/\s+/g, "").toUpperCase();
+      h2.className = "module";
+      container.appendChild(h2);
+      return;
+    }
     if (!line.includes("-")) {
       const h3 = document.createElement("h3");
       h3.textContent = line;
-      h3.className = "sections";
+      h3.className = "section";
       container.appendChild(h3);
       return;
     }
     const lastDash = line.lastIndexOf("-");
+    if (lastDash === -1) return;
+
     const en = line.slice(0, lastDash).trim();
     const ua = line.slice(lastDash + 1).trim();
 
     const p = document.createElement("p");
-    p.className = "words";
+    p.className = "word";
+    p.dataset.ua = ua;
     p.textContent = en;
 
     p.addEventListener("click", () => {
@@ -30,3 +53,4 @@ function renderWords(text) {
     container.appendChild(p);
   });
 }
+
