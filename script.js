767a77dae1a322ae011a74bb1a5e6fd513875718
Murosha <mz5145282@gmail.com>
2026-01-11 16:13:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,30 +17,43 @@ function renderWords(text) {
   const lines = text.split("\n");
 
   lines.forEach(line => {
+    line = line.trim();
+    if (!line) return;
+    if (/^\d+[A-Z]$/i.test(line)) {
+      const h2 = document.createElement("h2");
+      h2.textContent = line.toUpperCase();
+      h2.className = "module";
+      container.appendChild(h2);
+      return;
+    }
+
     if (!line.includes("-")) return;
 
-    const lastSlash = line.lastIndexOf("-");
-if (lastSlash === -1) return;
+    const lastDash = line.lastIndexOf("-");
+    if (lastDash === -1) return;
 
-const en = line.slice(0, lastSlash).trim();
-const ua = line.slice(lastSlash + 1).trim();
-
+    const en = line.slice(0, lastDash).trim();
+    const ua = line.slice(lastDash + 1).trim();
 
     const p = document.createElement("p");
     p.className = "word";
     p.dataset.ua = ua;
     p.textContent = en;
+
     p.addEventListener("click", () => {
-        p.textContent = p.textContent === en ? ua : en;
-       words.forEach(word => {
-  if (/\d/.test(word.textContent)) {
-    word.classList.add("big");
-  }
-  });
+      p.textContent = p.textContent === en ? ua : en;
     });
+
     container.appendChild(p);
   });
+  const words = document.querySelectorAll(".word");
+  words.forEach(word => {
+    if (/\d/.test(word.textContent)) {
+      word.classList.add("big");
+    }
+  });
 }
+
 
 
 
