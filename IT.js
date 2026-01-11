320f7c6a7bf0c90c908904457eb19c4c2d2cd881
Murosha <mz5145282@gmail.com>
2026-01-11 21:18:07 +0200

    Update IT.js

--- IT.js
+++ IT.js
@@ -1,21 +1,13 @@
 const btn = document.getElementById("btnIT");
 const itcontainer = document.getElementById("itwords");
 
-buttons.forEach(button => {
-  button.addEventListener("click", async () => {
-    const fileName = button.dataset.file;
-    console.log("Натиснув кнопку:", fileName)
-    const response = await fetch(fileName);
-    const text = await response.text();
-    console.log("Отриманний текст:", text);
-    renderWords(text);
-  });
-});
-
 function renderWords(text) {
   container.innerHTML = "";
   const lines = text.split("\n");
-    }
+
+  lines.forEach(line => {
+    line = line.trim();
+    if (!line) return;
     if (!line.includes("-")) {
       const h3 = document.createElement("h3");
       h3.textContent = line;
@@ -24,14 +16,11 @@ function renderWords(text) {
       return;
     }
     const lastDash = line.lastIndexOf("-");
-    if (lastDash === -1) return;
-
     const en = line.slice(0, lastDash).trim();
     const ua = line.slice(lastDash + 1).trim();
 
     const p = document.createElement("p");
     p.className = "words";
-    p.dataset.ua = ua;
     p.textContent = en;
 
     p.addEventListener("click", () => {
@@ -39,4 +28,5 @@ function renderWords(text) {
     });
 
     container.appendChild(p);
-  
+  });
+}
